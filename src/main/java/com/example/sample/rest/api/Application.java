b2579bd12952e0978f461e71084cd560a1c7a42b
wehwan <we_hwan@naver.com>
2020-10-13 21:52:14 +0900

    main class web-app not start

--- src/main/java/com/example/sample/rest/api/Application.java
+++ src/main/java/com/example/sample/rest/api/Application.java
@@ -1,13 +1,17 @@
 package com.example.sample.rest.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication springApplication = new SpringApplication(Application.class);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+
+		springApplication.run(args);
 	}
 
 }
