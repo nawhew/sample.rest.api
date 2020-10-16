09a71c0798b91af5fe575fc47e1a5c7eb0971ca0
nawhew <we_hwan@naver.com>
2020-10-16 14:51:46 +0900

    modify Event class to Entity

--- src/main/java/com/example/sample/rest/api/events/Event.java
+++ src/main/java/com/example/sample/rest/api/events/Event.java
@@ -2,27 +2,46 @@ package com.example.sample.rest.api.events;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Builder
-@Getter @Setter @ToString
-@AllArgsConstructor @NoArgsConstructor
-@EqualsAndHashCode(of = "id")
+@Entity
+@Getter @Setter @ToString @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
+
     private String name;
+
     private String description;
+
+    /*
+    * after spring-boot 2.1 support spring data jpa 2.3
+    * spring data jpa 2.3 provide auto type mapping : LocalDataTime */
     private LocalDateTime beginEnrollmentDateTime;
+
     private LocalDateTime closeEnrollmentDateTime;
+
     private LocalDateTime beginEventDateTime;
+
     private LocalDateTime endEventDateTime;
+
     private String location; // (optional) 이게 없으면 온라인 모임
+
     private int basePrice; // (optional)
+
     private int maxPrice; // (optional)
+
     private int limitOfEnrollment;
+
     private boolean offline;
+
     private boolean free;
+
     private EventStatus eventStatus;
 
 }
