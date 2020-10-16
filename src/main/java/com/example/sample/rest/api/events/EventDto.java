86b737ea9cab64c0d69dbd3679b906583b467dfc
nawhew <we_hwan@naver.com>
2020-10-16 15:37:49 +0900

    add dependency : modelmapper

---
+++ src/main/java/com/example/sample/rest/api/events/EventDto.java
@@ -0,0 +1,46 @@
+package com.example.sample.rest.api.events;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @ToString @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Event {
+
+    @Id @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    /*
+    * after spring-boot 2.1 support spring data jpa 2.3
+    * spring data jpa 2.3 provide auto type mapping : LocalDataTime */
+    private LocalDateTime beginEnrollmentDateTime;
+
+    private LocalDateTime closeEnrollmentDateTime;
+
+    private LocalDateTime beginEventDateTime;
+
+    private LocalDateTime endEventDateTime;
+
+    private String location; // (optional) 이게 없으면 온라인 모임
+
+    private int basePrice; // (optional)
+
+    private int maxPrice; // (optional)
+
+    private int limitOfEnrollment;
+
+    private boolean offline;
+
+    private boolean free;
+
+    @Enumerated(EnumType.STRING)
+    private EventStatus eventStatus;
+
+}
