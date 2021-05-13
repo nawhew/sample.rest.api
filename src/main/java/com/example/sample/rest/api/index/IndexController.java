e4b7730f187a978a8ce569f592c3300335b1c6f6
wehwan <we_hwan@naver.com>
2021-05-13 17:36:24 +0900

    feat: OAuth 로그인 추가

--- src/main/java/com/example/sample/rest/api/index/IndexController.java
+++ src/main/java/com/example/sample/rest/api/index/IndexController.java
@@ -1,8 +1,12 @@
 package com.example.sample.rest.api.index;
 
+import com.example.sample.rest.api.auth.LoginUser;
+import com.example.sample.rest.api.auth.dto.SessionUser;
 import com.example.sample.rest.api.events.web.EventController;
+import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,6 +14,21 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Controller
 public class IndexController {
+
+    private final EventController eventController;
+
+    public IndexController(EventController eventController) {
+        this.eventController = eventController;
+    }
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        // event list add attribute
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
 
     @GetMapping("/api")
     public @ResponseBody RepresentationModel index() {
